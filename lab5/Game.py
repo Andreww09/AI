11e10c18436c75c875b35f493772a80299a24d19
andradaAntoneac <antoneac.andrada.livia@gmail.com>
2023-11-09 12:24:53 +0200

    +minimax first draft

--- lab5/Game.py
+++ lab5/Game.py
@@ -1,11 +1,14 @@
+import copy
+
 from State import State
 
 
 class Game:
     def __init__(self):
-        # [2, 7, 6, 9, 5, 1, 4, 3, 8]
+        self.magic_square = [2, 7, 6, 9, 5, 1, 4, 3, 8]
         self.magic_square_index = [5, 0, 7, 6, 4, 2, 1, 8, 3]
         self.visited = [0] * 9
+        self.chosen_numbers = []
         self.state = State()
         self.winner = -1
 
@@ -47,7 +50,7 @@ class Game:
         player = state.get_last_player()
         moves = state.get_moves()
 
-        scores = [1, 10, 100]
+        scores = [0, 1, 10, 100]
         score = 0
 
         # verific liniile
@@ -70,7 +73,7 @@ class Game:
                 elif moves[3 * j + i] == 0:
                     cnt_empty += 1
             if cnt_player + cnt_empty == 3:
-                score += scores[cnt_player-1]
+                score += scores[cnt_player]
 
         # diagonala principala
         cnt_player = cnt_empty = 0
@@ -80,7 +83,7 @@ class Game:
             elif moves[3 * i + i] == 0:
                 cnt_empty += 1
         if cnt_player + cnt_empty == 3:
-            score += scores[cnt_player-1]
+            score += scores[cnt_player]
 
         # diagonala secundara
         cnt_player = cnt_empty = 0
@@ -90,21 +93,69 @@ class Game:
             elif moves[3 * i + 2 - i] == 0:
                 cnt_empty += 1
         if cnt_player + cnt_empty == 3:
-            score += scores[cnt_player-1]
+            score += scores[cnt_player]
 
         return score
+
+    def get_possible_values(self, chosen_numbers=None):
+        # aici ar trebui luata functia de valuate si de sortat lista returnata in functe de scorul mai mare
+        if chosen_numbers is None:
+            return [i+1 for i in range(len(self.visited)) if self.visited[i] != 0]
+        return [x for x in range(1, 10) if x not in chosen_numbers]
+
+    def minimax(self, depth, maximizing_player, state, chosen_numbers):
+        if depth == 0 or self.is_final(state):
+            return -1 if maximizing_player else 1
+
+        current_state = copy.deepcopy(state)
+        if maximizing_player:
+            max_eval = float('-inf')
+            for value1 in self.get_possible_values(chosen_numbers):
+                for value2 in [x for x in self.get_possible_values(chosen_numbers) if x != value1]:
+                    current_state.move(2, self.magic_square_index[value1 - 1])
+                    current_state.move(2, self.magic_square_index[value2 - 1])
+                    eval = self.minimax(depth-1, True, state, chosen_numbers + [value1, value2])
+                    max_eval = max(max_eval, eval)
+            return max_eval
+
+        else:
+            min_eval = float('inf')
+            for value1 in self.get_possible_values(chosen_numbers):
+                for value2 in [x for x in self.get_possible_values(chosen_numbers) if x != value1]:
+                    current_state.move(2, self.magic_square_index[value1 - 1])
+                    current_state.move(2, self.magic_square_index[value2 - 1])
+                    eval = self.minimax(depth - 1, False, state, chosen_numbers + [value1, value2])
+                    min_eval = min(min_eval, eval)
+            return min_eval
+
+    def ai_move(self):
+        best_move = None
+        max_eval = float('-inf')
+        for value1 in self.get_possible_values(self.chosen_numbers):
+            for value2 in [x for x in self.get_possible_values(self.chosen_numbers) if x != value1]:
+                eval = self.minimax(1, True, self.state, self.chosen_numbers + [value1, value2])
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = (value1, value2)
+        return best_move
 
     def start(self):
 
         player = 1
         while self.is_final(self.state) is False:
             print("Player", player, "'s turn")
-            move = int(input())
-            if self.move_is_valid(move) is False:
-                print("Invalid move")
-                continue
+            if player == 1:
+                move = int(input())
+                if self.move_is_valid(move) is False:
+                    print("Invalid move")
+                    continue
+            else:
+                move, _move = self.ai_move()
+                print(f"AI choose {move}")
+
             self.state.move(player, self.magic_square_index[move - 1])
             self.visited[move - 1] = 1
+            self.chosen_numbers.append(move)
 
             player = 3 - player
 
--- lab5/State.py
+++ lab5/State.py
@@ -1,18 +1,14 @@
-
 class State:
     def __init__(self):
-        self.moves=[0]*9
-        self.player=-1
+        self.moves = [0] * 9
+        self.player = -1
 
-    def move(self,player,pos):
-        self.moves[pos]=player
-        self.player=player
+    def move(self, player, pos):
+        self.moves[pos] = player
+        self.player = player
 
     def get_last_player(self):
         return self.player
 
     def get_moves(self):
         return self.moves
-
-
-
